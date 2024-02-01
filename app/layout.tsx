8c69db6dc84b3dd05485443187f7d316259a46f1
esthel <esthel9026@naver.com>
2024-02-02 02:36:30 +0900

    feat: loading 및 suspense 적용
    loading.tsx는 fetch 기다리는 동안 보여지는 화면
    suspense로 fetch 기다리는 동안 문구 출력

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-import Navigation from '../components/navigation';
+import { Navigation } from '../components';
 
 export const metadata: Metadata = {
   title: {
---
+++ components/index.tsx
@@ -0,0 +1,3 @@
+export { default as Navigation } from './navigation';
+export { default as MovieInfo } from './movie-info';
+export { default as MovieVideos } from './movie-videos';
