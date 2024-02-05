eb771037cf53917047f57bcc5810da0dc77beba1
esthel <esthel9026@naver.com>
2024-02-05 15:56:35 +0900

    feat: metadata 동적으로 설정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,11 +1,19 @@
 import { Suspense } from 'react';
 import { MovieInfo, MovieVideos } from '@components';
+import { getMovie } from '@components/movie-info';
 
-export default async function MovieDetail({
-  params: { id }
-}: {
+interface Params {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: Params) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title
+  };
+}
+
+export default async function MovieDetail({ params: { id } }: Params) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
