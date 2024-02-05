5cc5e2fecd390e6196b9866ef1359bd62ad29abd
esthel <esthel9026@naver.com>
2024-02-05 15:16:58 +0900

    design: css style
    styles import alias 설정

--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Navigation } from './navigation';
 export { default as MovieInfo } from './movie-info';
 export { default as MovieVideos } from './movie-videos';
+export { default as Movie } from './movie';
---
+++ components/movie.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import styles from '@styles/movie.module.css';
+
+interface MovieProps {
+  title: string;
+  id: string;
+  poster_path: string;
+}
+
+export default function movie({ title, id, poster_path }: MovieProps) {
+  const router = useRouter();
+  const showDetail = () => {
+    router.push(`/movies/${id}`);
+  };
+
+  return (
+    <div className={styles.movie}>
+      <img src={poster_path} alt={title} onClick={showDetail} />
+      <Link href={`/movies/${id}`}>{title}</Link>
+    </div>
+  );
+}
