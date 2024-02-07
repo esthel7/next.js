e7009c8cff5c2782915f3f976bbdba0a9c75a565
esthel <esthel9026@naver.com>
2024-02-08 05:26:46 +0900

    design: search css 완성
    언어 select 태그는 미완성

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,42 @@
+// 'use client';
+import styles from '@styles/search.module.css';
+
 export const metadata = {
   title: 'Search'
 };
 
 export default function Search() {
   return (
-    <div>
-      <h1>Search!</h1>
+    <div className={styles.search}>
+      <div
+        className={`${styles.container} ${styles.containerBox} ${styles.titleContainer}`}
+      >
+        <span>제목</span>
+        <div>
+          <input type="text" name="제목" placeholder="Type here..." />
+          <input type="submit" name="submit" value="✅" />
+        </div>
+      </div>
+
+      <div className={`${styles.containerBox} ${styles.containerGroup}`}>
+        <div className={`${styles.container} ${styles.miniContainer}`}>
+          <span>해당 별점 이상</span>
+          <input type="number" name="별점" placeholder="Number" />
+          <input type="submit" name="submit" value="✅" />
+        </div>
+
+        <div className={`${styles.container} ${styles.miniContainer}`}>
+          <span>언어</span>
+          <input type="text" name="언어" placeholder="Type here..." />
+          <input type="submit" name="submit" value="✅" />
+        </div>
+
+        <div className={`${styles.container} ${styles.miniContainer}`}>
+          <span>이후 개봉</span>
+          <input type="date" name="개봉" />
+          <input type="submit" name="submit" value="✅" />
+        </div>
+      </div>
     </div>
   );
 }
