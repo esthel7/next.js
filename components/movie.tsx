8222f0a564e99b79d8b1c3736c99351d7cb7302b
esthel <esthel9026@naver.com>
2024-02-12 03:56:17 +0900

    feat: tooltip 보이도록

--- components/movie.tsx
+++ components/movie.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useRef } from 'react';
 import styles from '@styles/movie.module.css';
 
 interface MovieProps {
@@ -12,14 +13,32 @@ interface MovieProps {
 
 export default function Movie({ title, id, poster_path }: MovieProps) {
   const router = useRouter();
+  const pRef = useRef<HTMLParagraphElement>(null);
+
   const showDetail = () => {
     router.push(`/movies/${id}`);
+  };
+
+  const tooltip = () => {
+    if (pRef.current) pRef.current.style.display = 'block';
+  };
+
+  const hideTooltip = () => {
+    if (pRef.current) pRef.current.style.display = 'none';
   };
 
   return (
     <div className={styles.movie}>
       <img src={poster_path} alt={title} onClick={showDetail} />
-      <Link href={`/movies/${id}`}>{title}</Link>
+      <p ref={pRef}>{title}</p>
+
+      <Link
+        href={`/movies/${id}`}
+        onMouseOver={tooltip}
+        onMouseOut={hideTooltip}
+      >
+        {title}
+      </Link>
     </div>
   );
 }
