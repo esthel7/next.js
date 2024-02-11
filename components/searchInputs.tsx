6068d3c4406542e7fa5b3296f2a2f484da82bdff
esthel <esthel9026@naver.com>
2024-02-11 19:33:13 +0900

    fix: recoil 상태 유지하도록

--- components/searchInputs.tsx
+++ components/searchInputs.tsx
@@ -1,12 +1,16 @@
 'use client';
 
-import { KeyboardEvent, useRef } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { KeyboardEvent, useEffect, useRef } from 'react';
+import { useRecoilState } from 'recoil';
 import { DateRecoil, StarRecoil, TitleRecoil } from '@states';
 
 export function TitleInput() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const setTitle = useSetRecoilState(TitleRecoil);
+  const [title, setTitle] = useRecoilState(TitleRecoil);
+
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.value = title;
+  }, []);
 
   const onSubmit = () => setTitle(inputRef.current.value);
 
@@ -25,7 +29,11 @@ export function TitleInput() {
 
 export function StarInput() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const setStar = useSetRecoilState(StarRecoil);
+  const [star, setStar] = useRecoilState(StarRecoil);
+
+  useEffect(() => {
+    if (inputRef.current && star !== 0) inputRef.current.value = String(star);
+  }, []);
 
   const validCheck = (event: KeyboardEvent<HTMLInputElement>) => {
     ['e', 'E', '+', '-', '.', '0'].includes(event.key) &&
@@ -54,7 +62,11 @@ export function StarInput() {
 
 export function DateInput() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const setDate = useSetRecoilState(DateRecoil);
+  const [date, setDate] = useRecoilState(DateRecoil);
+
+  useEffect(() => {
+    if (inputRef.current && date !== '') inputRef.current.value = date;
+  }, []);
 
   const onSubmit = () => setDate(inputRef.current.value);
 
