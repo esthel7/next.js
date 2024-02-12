68c0d5a1860d9c7e1df71761d637f37ae08eb140
esthel <esthel9026@naver.com>
2024-02-13 04:38:09 +0900

    design: edit movieInfo css

--- components/movieInfo.tsx
+++ components/movieInfo.tsx
@@ -12,7 +12,7 @@ export default async function MovieInfo({ id }: { id: string }) {
         alt={movie.title}
       />
       <div className={styles.info}>
-        <h1 className={styles.title}>{movie.title}</h1>
+        <h1>{movie.title}</h1>
         <h3>⭐{movie.vote_average.toFixed(1)}</h3>
         <p>{movie.overview}</p>
         <a href={movie.homepage} target={'_blank'}>
