e098507fe8ccdb2fba6a7033821ec45b485b776f
esthel <esthel9026@naver.com>
2024-02-15 02:41:42 +0900

    refactor: tailwind navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,18 +2,18 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import styles from '@styles/navigation.module.css';
 
 export default function Navigation() {
   const path = usePathname();
 
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li>
+    <nav className="bg-darkGray fixed top-8 w-1/3 mx-auto border border-white rounded-full py-3 z-10 left-1/2 -translate-x-1/2 176:w-52">
+      <ul className="flex justify-center gap-7 mx-5">
+        <li className="transition-all duration-100 ease-in-out hover:scale-105">
           <Link href="/">Home</Link> {path === '/' ? '🍿' : ''}
         </li>
-        <li>
+
+        <li className="transition-all duration-100 ease-in-out hover:scale-105">
           <Link href="/search">Search</Link>
           {path === '/search' ? '🔍' : ''}
         </li>
