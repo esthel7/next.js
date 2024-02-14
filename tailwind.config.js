1718b1f7372f53748dcc54d0fe635be06c54c5b2
esthel <esthel9026@naver.com>
2024-02-14 21:53:33 +0900

    refactor: tailwind movieVideo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./app/**/*.{ts,tsx,js,tsx}', './components/**/*.{ts,tsx,js,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      gridTemplateColumns: {
+        video: 'repeat(auto-fit, minmax(250px, 1fr))'
+      },
+      spacing: {
+        144: '36rem'
+      },
+      screens: {
+        144: { max: '36rem' }
+      }
+    }
   },
   plugins: []
 };
