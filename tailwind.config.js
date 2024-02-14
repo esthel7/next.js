357033a764fa156ec950ca923ae6b73f76b21f87
esthel <esthel9026@naver.com>
2024-02-14 19:10:09 +0900

    add: tailwind
    global.css 초기 생성

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{ts,tsx,js,tsx}', './components/**/*.{ts,tsx,js,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
