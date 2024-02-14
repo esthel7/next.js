b79c158653802eac3b235bfc4bfb3d4508d8f044
esthel <esthel9026@naver.com>
2024-02-15 04:03:15 +0900

    refactor: tailwind home
    @layer components로 변수화 진행, media태그를 여러개 사용할 경우 범위 지정 필요(min, max)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,6 @@
 import { useGetMovies } from '@hooks';
 import { Movie } from '@components';
 import { MovieType } from '@states';
-import styles from '@styles/home.module.css';
 
 export const metadata = {
   title: 'Home'
@@ -12,7 +11,7 @@ export default async function HomePage() {
   const movies = await getMovies();
 
   return (
-    <div className={styles.container}>
+    <div className="homeContainer">
       {movies.map((movie: MovieType) => (
         <Movie
           key={movie.id}
