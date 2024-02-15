7c918759019b518c18f402d0b1385eafb9c9251b
esthel <esthel9026@naver.com>
2024-02-16 04:47:38 +0900

    fix: navigation 이동 시 scale 유지

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -7,14 +7,25 @@ export default function Navigation() {
   const path = usePathname();
 
   return (
-    <nav className="bg-darkGray fixed top-8 w-1/3 mx-auto border border-white rounded-full py-3 z-10 left-1/2 -translate-x-1/2 176:w-52">
+    <nav className="bg-darkGray fixed top-8 w-1/3 mx-auto border border-white rounded-full py-3 z-10 left-1/2 -translate-x-1/2 176:w-56">
       <ul className="flex justify-center gap-7 mx-5">
         <li className="transition-all duration-100 ease-in-out hover:scale-105">
-          <Link href="/">Home</Link> {path === '/' ? '🍿' : ''}
+          <Link
+            href="/"
+            className={path === '/' ? 'text-xl opacity-100' : null}
+          >
+            Home
+          </Link>
+          {path === '/' ? '🍿' : ''}
         </li>
 
         <li className="transition-all duration-100 ease-in-out hover:scale-105">
-          <Link href="/search">Search</Link>
+          <Link
+            href="/search"
+            className={path === '/search' ? 'text-xl opacity-100' : null}
+          >
+            Search
+          </Link>
           {path === '/search' ? '🔍' : ''}
         </li>
       </ul>
