7b4facec0e0afee379e97b4e3fb5ae5eeb36c85e
esthel <esthel9026@naver.com>
2024-02-27 21:15:29 +0900

    feat: iframe background img로 로딩이미지 구현

--- components/movieVideos.tsx
+++ components/movieVideos.tsx
@@ -23,7 +23,7 @@ export default async function MovieVideos({ id }: { id: string }) {
     <div className="mx-auto mt-24 pb-24 grid grid-cols-video gap-5">
       {videos.map((video: Video) => (
         <iframe
-          className="w-auto h-auto mx-auto max-w-full rounded-lg opacity-80 transition-opacity duration-200 ease-in-out hover:opacity-100"
+          className="w-auto h-auto mx-auto max-w-full rounded-lg opacity-80 transition-opacity duration-200 ease-in-out bg-[url('../public/loading.gif')] bg-no-repeat bg-center hover:opacity-100"
           key={video.id}
           src={`https://youtube.com/embed/${video.key}`}
           title={video.name}
